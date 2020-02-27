f035e6022de8c7979ba46257e183eb860fa30f4b
morrisjdev <morrisj@live.de>
2020-02-27 16:05:50 +0100

    added SubscriptionManager structure

--- SapphireDb/Connection/SubscriptionManager.cs
+++ SapphireDb/Connection/SubscriptionManager.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using SapphireDb.Models;
 
 namespace SapphireDb.Connection
 {
     public class SubscriptionManager
     {
-        private ConcurrentDictionary<SubscriptionGroupingKey, string> subscriptions = new ConcurrentDictionary<SubscriptionGroupingKey, string>();
+        private ReaderWriterLockSlim readerWriterLockSlim = new ReaderWriterLockSlim();
+        private ConcurrentDictionary<SubscriptionGroupingKey, List<CollectionSubscription>> subscriptions =
+            new ConcurrentDictionary<SubscriptionGroupingKey, List<CollectionSubscription>>();
+
+        public void AddSubscription(CollectionSubscription subscription)
+        {
+            readerWriterLockSlim.EnterWriteLock();
+
+            // Add to dictionary (or create lookup?)
+            
+            readerWriterLockSlim.ExitWriteLock();
+        }
     }
 
-    class SubscriptionGroupingKey
+    class SubscriptionGroupingKey : IEquatable<SubscriptionGroupingKey>
     {
         public Type DbContextType { get; set; }
 
         public string CollectionName { get; set; }
 
-        public string PrefilterHash { get; set; }
+        public bool RequiresDatabaseQuery { get; set; }
+        // public string PrefilterHash { get; set; }
+
+        public bool Equals(SubscriptionGroupingKey other)
+        {
+            return DbContextType == other?.DbContextType && CollectionName == other?.CollectionName &&
+                   RequiresDatabaseQuery == other?.RequiresDatabaseQuery;
+        }
     }
 }
