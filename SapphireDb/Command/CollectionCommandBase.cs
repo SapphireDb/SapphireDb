c99d598062c151431027ae6d6b9ff5e37d53d083
morrisjdev <morrisj@live.de>
2020-05-08 19:48:13 +0200

    removed splitting of collection name on client side

--- SapphireDb/Command/CollectionCommandBase.cs
+++ SapphireDb/Command/CollectionCommandBase.cs
@@ -2,7 +2,39 @@
 {
     public class CollectionCommandBase : CommandBase
     {
-        public string CollectionName { get; set; }
-        public string ContextName { get; set; } = "default";
+        private string collectionName;
+        private string contextName = "default";
+
+        public string CollectionName
+        {
+            get
+            {
+                return collectionName;
+            }
+            set
+            {
+                string[] collectionNameParts = value.Split('.');
+
+                if (collectionNameParts.Length == 1)
+                {
+                    collectionName = collectionNameParts[0];
+                }
+                else if (collectionNameParts.Length == 2)
+                {
+                    contextName = collectionNameParts[0];
+                    collectionName = collectionNameParts[1];
+                }
+                else
+                {
+                    collectionName = value;
+                }
+                
+            }
+        }
+
+        public string ContextName
+        {
+            get { return contextName; }
+        }
     }
 }
