837d2f608c8efe5c8e76d21ae04eb7046cdf631a
morrisjdev <morrisj@live.de>
2020-07-24 17:33:42 +0200

    added sending of exception data in error response

--- SapphireDb/Models/Exceptions/UnhandledException.cs
+++ SapphireDb/Models/Exceptions/UnhandledException.cs
@@ -6,7 +6,12 @@ namespace SapphireDb.Models.Exceptions
     {
         public UnhandledException(Exception exception) : base("An unhandled exception was thrown.", exception)
         {
-            
+            UnhandledType = exception.GetType().Name;
+            UnhandledMessage = exception.Message;
         }
+
+        public string UnhandledType { get; }
+
+        public string UnhandledMessage { get; }
     }
 }
--- SapphireDb/Models/SapphireDbErrorResponse.cs
+++ SapphireDb/Models/SapphireDbErrorResponse.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using SapphireDb.Models.Exceptions;
 
 namespace SapphireDb.Models
@@ -10,12 +13,18 @@ namespace SapphireDb.Models
             Type = exception.GetType().Name;
             Message = exception.Message;
             Id = exception.Id;
+
+            Data = exception.GetType()
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                .ToDictionary(property => property.Name, property => property.GetValue(exception));
         }
 
         public string Type { get; }
 
         public string Message { get; }
 
-        public Guid Id { get; set; }
+        public Guid Id { get; }
+
+        public Dictionary<string, object> Data { get; }
     }
 }
