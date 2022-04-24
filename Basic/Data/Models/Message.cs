dc79b3169e2ae85828cac5d4b4920b161fed0999
Morris Janatzek <morrisj@live.de>
2022-04-24 13:21:40 +0200

    added cors settings made message content updatable

--- Basic/Data/Models/Message.cs
+++ Basic/Data/Models/Message.cs
@@ -1,6 +1,9 @@
-﻿namespace Basic.Data.Models;
+﻿using SapphireDb.Attributes;
+
+namespace Basic.Data.Models;
 
 public class Message : Base
 {
+    [Updateable]
     public string Content { get; set; }
 }
--- Basic/Program.cs
+++ Basic/Program.cs
@@ -4,11 +4,15 @@ using SapphireDb.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
+
 builder.Services
     .AddSapphireDb()
     .AddContext<BasicDbContext>(cfg => cfg.UseInMemoryDatabase("basic"));
 
 var app = builder.Build();
+
+app.UseCors(cfg => cfg.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(origin => true).AllowCredentials());
 
 app.UseSapphireDb();
 
